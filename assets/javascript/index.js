0e5598ec1ff0ef8817336a59dfc5c7559c2662c9
Faras Momin <famomin@utexas.edu>
2016-12-10 20:44:32 -0600

    firebase, checkrate, and comparison page js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,5 @@
+ var userZip= ""; //using for Comparisoin Page and check rate page
+
  //-------------------------------------------
  //-----------JS for Homepage ----------------
  //------------------------------------------- 
@@ -35,5 +37,48 @@ $('#subscribeNewsletter').on("click", function(){
 
 
  //-------------------------------------------
- //-----------JS for PAGE 2 - ----------------
+ //--------JS for Contact Use pade -----------
  //------------------------------------------- 
+//  $('#messageSent').on("click", function(){
+// 	var name = $('#nameContact').val().trim();
+// 	var email= $('#emailContact').val().trim();
+// 	var phone = $('#phoneContact').val().trim();
+// 	var message = $('#messageContact').val().trim();
+	
+// 		database.ref().push({
+// 			name: name,
+// 			email: email,
+// 			phone: phone,
+// 			message: message
+// 		});	
+// 	name.empty();
+
+// 	return false;
+// });
+
+
+//-------------------------------------------
+//--------JS for check rates ----------------
+//-------------------------------------------
+
+$('.checkRatesButton').on("click", function(){
+	event.preventDefault();
+
+	userZip = $('#shipsFrom').val();
+
+});
+
+
+//-------------------------------------------
+//--------JS for Comparison Page ------------
+//-------------------------------------------
+$(document).ready(function() {
+	var userdestin = $('#shipsTo').val();
+	var shippingCarrier = "USPS";
+
+	var xxxyyyzzz = $('#showMap').html('<iframe width="1000" height="900" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q='+shippingCarrier+',77479&key=AIzaSyBn7OO0R_3Er16AAeAkJWdVspW2u7tNMmg" allowfullscreen></iframe>');
+	console.log(userZip);
+	console.log(userdestin);
+	console.log(shippingCarrier);
+	console.log(xxxyyyzzz);
+});
