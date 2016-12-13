983de1abeda4c53b0f85ba76987160ed0d8727ef
Azim Momin <azimmomin123@gmail.com>
2016-12-12 19:58:23 -0600

    app.js is working file passing infromation to the api. All that is left to do is pull user information via jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,53 @@
-// var parcel = {
-//     // in INCHES
-//     length: 20,
-//     width: 15,
-//     height: 10,
-//     // in OZ
-//     weight: 45
-// };
 
 
-// parcel = JSON.stringify(parcel);
-var weight = 100;
-var length = 101;
+//User Parcel Data Input(Uncomment when working)
 
+
+
+//User parcel data
+var length = 20;
+var width = 30;
+var height = 43;
+var weight = 21.9;
+
+//User toAddress data 
+var toStreet = "7037 Talbot Street "; 
+var toCity = "Freeport"; 
+var toState = "New York"; 
+var toZipcode = "11520";
+var toCountry = "US";
+var toPhone = "281-265-6073";  
+
+//User fromAddress data 
+var fromStreet = "4626 Russett Lane"; 
+var fromCity = "Sugar Land"; 
+var fromState = "TX"; 
+var fromZipcode = "77479";
+var fromCountry = "US";
+var fromPhone = "281-265-1111";  
 
 //Heroku Link
-var queryURL = "https://nameless-inlet-48347.herokuapp.com/"+ weight + "/" + length;
+var queryURL = "https://nameless-inlet-48347.herokuapp.com/"
+//Parcel Data
++ length 			
++ "/" + width 		
++ "/" + height		
++ "/" + weight
+//toAddress		
++ "/" + toStreet 
++ "/" + toCity
++ "/" + toState
++ "/" + toZipcode 
++ "/" + toCountry
++ "/" + toPhone
+//toAddress		
++ "/" + fromStreet 
++ "/" + fromCity
++ "/" + fromState
++ "/" + fromZipcode 
++ "/" + fromCountry
++ "/" + fromPhone;
+
 
 //Local Test Link
 //var queryURL = 'localhost:3000';
@@ -28,8 +61,10 @@ $.ajax({ url: queryURL, method: 'GET'})
 	console.log(results);
 
 	var a = results.rates[1].rate;
+	//Variable for parcel data
 	var b = results.parcel;
-	console.log(a);
-	console.log(b);
-	$("#test").html(a);
+	//console.log(a);
+	//Prints Parcel data
+	//console.log(b);
+	
 })
