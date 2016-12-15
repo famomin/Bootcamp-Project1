5ebc26d3e76382e20b52b38aa77375e5f27d383a
Azim Momin <azimmomin123@gmail.com>
2016-12-14 21:50:30 -0600

    added hard coded addresses for testing purposes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,51 +2,58 @@ $('#checkRatesButton').on("click", function(event){
 	
 	event.preventDefault();
 
-	//User parcel Input data
-	var length = $('#LengthFrom').val();
-	var width = $('#WidthFrom').val();
-	var height = $('#HeightFrom').val();
+	//user weight data
+	var length = 2;
+	var width = 2;
+	var height = 2;
+	var weight = 32;
 
-	//API accepts weight in OZ
-	var lb = parseInt($('#weightFormlb').val()); 
-	//Converting lb to oz
-	var lbTOoz = lb * 16;
-	//OZ input value
-	var oz = parseInt($('#weightFormOZ').val());
-	//Weight in OZ
-	var weight = lbTOoz + oz;
+
+	//User parcel Input data
+	// var length = $('#LengthFrom').val();
+	// var width = $('#WidthFrom').val();
+	// var height = $('#HeightFrom').val();
+
+	// //API accepts weight in OZ
+	// var lb = parseInt($('#weightFormlb').val()); 
+	// //Converting lb to oz
+	// var lbTOoz = lb * 16;
+	// //OZ input value
+	// var oz = parseInt($('#weightFormOZ').val());
+	// //Weight in OZ
+	// var weight = lbTOoz + oz;
 
 	//User toAddress data 
-	// var toStreet = "7037 Talbot Street "; 
-	// var toCity = "Freeport"; 
-	// var toState = "New York"; 
-	// var toZipcode = "11520";
-	// var toCountry = "US";
-	// var toPhone = "281-265-6073";  
-
-	//User toAddress Input data 
-	var toStreet = $('#toStreetAdd').val(); 
-	var toCity = $('#toCityAdd').val();
-	var toState = $('#toStateAdd').val(); 
-	var toZipcode = $('#toZipAdd').val();
+	var toStreet = "7037 Talbot Street "; 
+	var toCity = "Freeport"; 
+	var toState = "New York"; 
+	var toZipcode = "11520";
 	var toCountry = "US";
 	var toPhone = "281-265-6073";  
 
-	//User fromAddress data 
-	// var fromStreet = "4626 Russett Lane"; 
-	// var fromCity = "Sugar Land"; 
-	// var fromState = "TX"; 
-	// var fromZipcode = "77479";
-	// var fromCountry = "US";
-	// var fromPhone = "281-265-1111";  
+	//User toAddress Input data 
+	// var toStreet = $('#toStreetAdd').val(); 
+	// var toCity = $('#toCityAdd').val();
+	// var toState = $('#toStateAdd').val(); 
+	// var toZipcode = $('#toZipAdd').val();
+	// var toCountry = "US";
+	// var toPhone = "281-265-6073";  
 
-	//User fromAddress Input data 
-	var fromStreet = $('#fromStreetAdd').val(); 
-	var fromCity = $('#fromCityAdd').val();
-	var fromState = $('#fromStateAdd').val(); 
-	var fromZipcode = $('#fromZipAdd').val();
+	//User fromAddress data 
+	var fromStreet = "4626 Russett Lane"; 
+	var fromCity = "Sugar Land"; 
+	var fromState = "TX"; 
+	var fromZipcode = "77479";
 	var fromCountry = "US";
 	var fromPhone = "281-265-1111";  
+
+	//User fromAddress Input data 
+	// var fromStreet = $('#fromStreetAdd').val(); 
+	// var fromCity = $('#fromCityAdd').val();
+	// var fromState = $('#fromStateAdd').val(); 
+	// var fromZipcode = $('#fromZipAdd').val();
+	// var fromCountry = "US";
+	// var fromPhone = "281-265-1111";  
 
 	//Heroku Link
 	var queryURL = "https://nameless-inlet-48347.herokuapp.com/"
